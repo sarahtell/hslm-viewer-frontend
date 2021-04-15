047dd7a7cf68617dec184a696375c3ccf23def13
sarahtell <vtell@kth.se>
2021-04-15 14:03:24 +0200

    add form

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import axios from "axios"
 import React from "react"
+import Form from './components/Form'
 
 const App = () => {
 
-  const [isLoading, setIsLoading] = React.useState(true)
   const [data, setData] = React.useState({})
 
-  React.useEffect(() => {
-    const response = axios
-    .get('http://127.0.0.1:5000/', {headers: "Bearer 12903109230+912309+1" })
-    .then(response => {
-      console.log(response)
-      setData(response.data)
-
-    })
-    setIsLoading(false)
-    console.log(response)
-  },[]);
-
-console.log(process.env.REACT_APP_SARAH)
-  if (isLoading) return <h1>Loading...</h1>
-
   return (
-    <div className="text-blue-300 text-7xl">
-    HELLO A FRONTEND! aswdjklajsdjk alskd
-    {data.hej}
+    <div className="flex flex-col items-center">
+    HELLO A FRONTEND!
+    <Form setData={setData}/>
+    {/* <Graph data={data}/> */}
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,81 @@
+import { useForm } from "react-hook-form";
+import axios from "axios"
+
+
+function getModalProperties(formData) {
+    console.log(formData);
+    const response = axios
+    .get('http://127.0.0.1:5000/', {headers: "Bearer 12903109230+9123091" })
+    .then(response => {
+      return response.data
+    })
+    return response
+}
+
+
+export default function Form({ setData }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  
+  const onSubmit = (formData) => {
+
+    const data = getModalProperties(formData)
+
+    setData(data);
+
+  }
+
+
+
+
+  const properties = {
+    mass: { name: "Massa [kg/m]", value: 19000 },
+    youngs_modulus: { name: "E-modul []", value: 200e9 },
+    moment_of_inertia: { name: "Tröghetsmoment []", value: 0.86 },
+    damping_ratio: { name: "Dämpkvot", value: 0.005 },
+    length: { name: "Längd [m]", value: 48.0 },
+    element_size: { name: "Elementstorlek [m]", value: 0.1 },
+    mode_numbers: { name: "Antal moder [-]", value: [1, 2, 3] },
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col py-5 space-y-5">
+        {Object.entries(properties).map(([propertyName, props]) => {
+          return (
+            <div className="flex flex-col">
+              <p className="text-black text-xs">{props.name}</p>
+
+              <input
+                className="border border-solid"
+                type="text"
+                value={props.value}
+                placeholder={props.value}
+                {...register(propertyName, {
+                  required: "Fält måste vara ifyllt.",
+                })}
+              />
+
+              {errors[propertyName] ? (
+                <p className="text-red-500 text-xs">
+                  {errors[propertyName].message}
+                </p>
+              ) : null}
+
+            </div>
+          );
+        })}
+
+        <button
+          className="bg-green-500 text-white p-2 rounded-md"
+          type="submit"
+        >
+          Beräkna
+        </button>
+      </div>
+    </form>
+  );
+}
