0c2dc6b2d757c756d6830633091759607d58c7ca
sarahtell <vtell@kth.se>
2021-04-27 17:55:11 +0200

    changes to layout in startpage

--- src/Startpage.js
+++ src/Startpage.js
@@ -3,13 +3,14 @@ import Layout from "./components/Layout";
 import pdf from "./components/cv.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Startpage = () => {
   return (
     <Layout>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col h-full items-center">
         <img
-          className="w-1/3 rounded-full m-10"
+          className="w-1/3 max-w-md rounded-full m-10"
           src="./Profilepic.jpg"
           alt="SARAH TELL"
         ></img>
@@ -17,11 +18,20 @@ const Startpage = () => {
         <h1 className="flex flex-col items-center font-sans text-5xl m-5">
           Sarah Tell
         </h1>
-        <h2 className="mt-5 text-4xl">Jag gillar kod</h2>
-        <a className="mt-5" href={pdf}>Download CV</a>
+        <h2 className="mt-5 text-4xl justify-center"> I like coding.</h2>
+        <h3 className="flex flex-col justify-center mt-5">
+          Head to{" "}
+          <Link className="text-gray-700 font-bold" to="/bridgeresponse">
+            Projects
+          </Link>{" "}
+          to check out my bridge response simulator for passing trains.
+        </h3>
+        <a className="bg-gray-700 text-white p-3 mt-20 rounded-md" href={pdf}>
+          Download CV
+        </a>
       </div>
-      <div className="flex absolute w-full bottom-0 bg-gray-700 justify-center">
-        <a className="p-5" href="https://www.linkedin.com/in/sarah-tell-90/">
+      <div className="flex fixed w-full bottom-0 bg-gray-700 justify-center">
+        <a className="m-5" href="https://www.linkedin.com/in/sarah-tell-90/">
           <FontAwesomeIcon size={"2x"} icon={faLinkedinIn} color="white" />
         </a>
       </div>
