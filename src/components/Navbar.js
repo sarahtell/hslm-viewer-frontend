21b5f93334ca4d58250a3059fb8f312165df9ae1
sarahtell <sarah.lm.tell@gmail.com>
2022-05-04 22:50:39 +0200

    remove build + fix responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-700 text-white flex justify-between w-full">
+    <nav className="bg-gray-700 text-white h-44 flex-col flex md:flex-row md:justify-between items-center w-full overflow-x-hidden">
       <Link className="p-3 whitespace-nowrap md:mx-20" to="/">
         Sarah Tell
       </Link>
